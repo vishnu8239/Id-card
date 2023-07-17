1e92906b1451b2dea9ac875e556bcfa26158cd97
vishnu8239 <vishnu.20jics072@jietjodhpur.ac.in>
2023-07-17 14:35:39 +0530

    Idcard

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Image, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.avatarContainer}>
+          <Image style={styles.avatar} source={require('./src/v.jpg')} />
+        </View>
+        
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>VISHNU JANGID</Text>
+          <Text style={styles.title}>Employee ID: 8239034523</Text>
+          <Text style={styles.title}>Department: Engineering</Text>
+          
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+    paddingHorizontal: 16,
   },
+  avatarContainer: {
+    alignItems: 'stretch',
+    justifyContent: 'space-evenly',
+    marginBottom: 16,
+  },
+  avatar: {
+    width: 200,
+    height: 300,
+    borderRadius: 10,
+    marginBottom: 30,
+    
+  },
+  infoContainer: {
+    
+    marginHorizontal: 10,
+  },
+  title: {
+    marginTop: 20,
+    fontSize: 18,
+    marginBottom: 20,
+  },
+
+
 });
+
+export default App;
